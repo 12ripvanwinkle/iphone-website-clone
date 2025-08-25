6835bbb489436597e0b80eba0180cd5a11adba87
Nitesh <niteshanthony3@gmail.com>
2025-08-25 17:38:13 -0500

    completed the Navbar section

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import { navLists } from '../constants';
+import {appleImg, bagImg, searchImg} from '../utils';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
+        <img src={appleImg} alt='Apple' width={14} height={18}/>
+
+        <div className='flex flex-1 justify-center max-sm:hidden gap-8'>
+          {navLists.map((nav) => (
+            <div key={nav} className="text-sm cursor-pointer text-gray-600 hover:text-white transition-colors duration-300">
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt='Search' width={18} height={18}/>
+          <img src={bagImg} alt='Bag' width={18} height={18}/>
+        </div>
+
+      </nav>
+    </header>
   )
 }
 
