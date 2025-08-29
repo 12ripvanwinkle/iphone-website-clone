3517d256d9fed25390ca40c2b0e4b340c35ba193
Nitesh <niteshanthony3@gmail.com>
2025-08-29 18:47:58 -0500

    implemented sentry to track website data and started the features sections

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e9cad86aa882ed699f3c432eb36ead2b@o4509929724248064.ingest.us.sentry.io/4509929726148608",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
